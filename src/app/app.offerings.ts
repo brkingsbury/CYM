b10768f2baa6feb47036a197a83d0cef57505b76
Brian <briankingsbury1@gmail.com>
2022-09-20 17:04:29 -0500

    Convert offerings into 1 source, update pages

--- src/app/app.offerings.ts
+++ src/app/app.offerings.ts
@@ -1,86 +1,97 @@
-export const currentOfferings = [
-          {
-            className: 'Yoga with Your Little',
-            classType: 'Class',
-            classDescription: '',
-            classTime: '9am',
-            classPrice: '18 (for 1 adult & 1 child)',
-            classColor: 'green',
-            classLocation: 'Integrative Wellness - Bay View',
-            classAddress: '',
-            classAddressURL: '',
-            classPicure: '',
-            classSignUp: true,
-            classCalendlyURL: '',
-            classDates: [
-              '09/17/2022',
-              '10/01/2022',
-              '08/18/2022', //test
-              '10/15/2022',
-              '10/29/2022'
-            ]
-          },
-          {
-            className: 'Yoga for Parents + Caregivers',
-            classType: 'Class',
-            classDescription: '',
-            classTime: '9am',
-            classPrice: '15 (for 1 adult & children)',
-            classColor: 'orange',
-            classLocation: 'Heart Revival Center - St Francis',
-            classAddress: '',
-            classAddressURL: '',
-            classPicure: '',
-            classSignUp: true,
-            classCalendlyURL: '',
-            classDates: [
-                '09/23/2022',
-                '09/30/2022',
-                '10/07/2022',
-                '08/16/2022', //test
-                '10/14/2022',
-                '10/21/2022',
-                '10/28/2022'
-              ]
-          },
-          {
-            className: "Kids' Yoga in the Park",
-            classType: 'Event',
-            classDescription: '',
-            classTime: '10:30am',
-            classPrice: 'FREE',
-            classColor: 'gold',
-            classLocation: "Alice's Garden",
-            classAddress: '',
-            classAddressURL: '',
-            classPicure: '',
-            classSignUp: false,
-            classCalendlyURL: '',
-            classDates: [
-                '09/24/2022'
-              ]
-          },
-        ]
+export interface Offerings {
+  name: string;
+  type: string;
+  description: string;
+  time: string;
+  length: string;
+  price: string;
+  color: string;
+  location: string;
+  address: string;
+  addressUrl: string;
+  picture: string;
+  signUp: boolean;
+  url?: string;
+  calendlyUrl?: string;
+  classDates: string[];
+}
 
-
-// {
-//     name: "Yoga with Your Little",
-//     description: "Come practice connecting with your little one (ages 3-7) in this 45 minute yoga class. This class will combine yoga poses (asana), breathing practices (pranayama), and fun games/activities to help children and adults cultivate mindful awareness, emotion regulation strategies, and a sense of joy and gratitude for each other!",
-//     color: "green",
-//     price: "18 (for 1 adult & 1 child)",
-//     location: "Integrative Wellness Yoga Shala",
-//     address: "3201 S Howell Ave, Milwaukee, WI 53207",
-//     addressURL: "https://goo.gl/maps/hQvrdqHATAmkGB8H7",
-//     time: "45min",
-//     picture: "../../assets/ywl_1.jpg",
-//     calendly: true,
-//     url: "https://calendly.com/cultivateyogamke/yogawithyourlittle"
-//   },
-
-// { style: 'green', 
-// classDate: '09/17/2022', 
-// className: 'Yoga with Your Little', 
-// classTime: '9am', 
-// location: 'Integrative Wellness - Bay View', 
-// signUp: true, 
-// url: "https://calendly.com/cultivateyogamke/yogawithyourlittle" },
+export const currentOfferings: Offerings[] = [
+  {
+    name: 'Yoga with Your Little',
+    type: 'Class',
+    description: 'Come practice connecting with your little one (ages 3-7) in this 45 minute yoga class. This class will combine yoga poses (asana), breathing practices (pranayama), and fun games/activities to help children and adults cultivate mindful awareness, emotion regulation strategies, and a sense of joy and gratitude for each other!',
+    time: '9am',
+    length: '45min',
+    price: '18 (for 1 adult & 1 child)',
+    color: 'green',
+    location: 'Integrative Wellness - Bay View',
+    address: '3201 S Howell Ave, Milwaukee, WI 53207',
+    addressUrl: 'https://goo.gl/maps/hQvrdqHATAmkGB8H7',
+    picture: '../assets/ywl_1.jpg',
+    signUp: true,
+    calendlyUrl: 'https://calendly.com/cultivateyogamke/yogawithyourlittle',
+    classDates: [] = [
+      '10/01/2022',
+      '10/15/2022',
+      '10/29/2022'
+    ]
+  },
+  {
+    name: 'Yoga for Parents + Caregivers',
+    type: 'Class',
+    description: "A yoga class that you can bring your kid to! All levels are welcome to this class that will focus on self-care and themes around parenting / caregiving. Classes will be geared toward the adults while the little ones can watch and follow along by your side or play with quiet toys." + "\n\n" + " Perfect for stay-at-home parents looking for a space to practice together and meet other parents. Guardians and caregivers of all types are welcome. We will use distractions by the kids as an opportunity to practice mindfulness and understanding. No limit for age or number of kids. A few quiet toys will be provided, but feel free to bring your own as well. Let's practice letting go and enjoy our yoga practice in community!",
+    time: '9am',
+    length: '45min',
+    price: '15 (for 1 adult & children)',
+    color: 'orange',
+    location: 'The Heart Revival Center for Yoga and Buddhist Studies',
+    address: '3701 S Packard Ave, St Francis, WI 53235',
+    addressUrl: 'https://goo.gl/maps/QuWPrgYKRTf54QGy7',
+    picture: '../assets/y4p_1.jpg',
+    signUp: true,
+    calendlyUrl: 'https://calendly.com/cultivateyogamke/yogaforparents',
+    classDates: [] = [
+      '09/23/2022',
+      '09/30/2022',
+      '10/07/2022',
+      '10/14/2022',
+      '10/21/2022',
+      '10/28/2022'
+    ]
+  },
+  {
+    name: "Kids' Yoga in the Garden",
+    type: 'Event',
+    description: "Join us in the garden for a yoga class for kids of all ages! In this 45 minute session, we'll use the beautiful surroundings of the garden and the tools of yoga to practice connecting with the breath, the body, and with nature. " + "\n\n" + " Adults are welcome to practice alongside their little ones or watch from the sidelines - whatever is most comfortable for you. Feel free to bring a mat (or 2) or blanket to practice on... or just practice on the grass if you prefer!",
+    length: '45min',
+    time: '10:30am',
+    price: 'FREE',
+    color: 'gold',
+    location: "Alice's Garden Urban Farm",
+    address: '2136 N 21st St, Milwaukee, WI 53205',
+    addressUrl: 'https://goo.gl/maps/hLGsSQ4BzMjoYNGH6',
+    picture: '',
+    signUp: false,
+    url: 'https://www.facebook.com/events/3096932107190182/?ref=newsfeed',
+    classDates: [] = [
+      '09/24/2022'
+    ]
+  },
+  {
+    name: "Yoga Buddies",
+    type: 'Other',
+    description: "This is a space for little ones, ages 2-5 and one caregiver to practice yoga together. Using the tools of yoga, we will practice skills to handle big feelings. Each class will combine yoga poses (asana) and breathing techniques (pranayama) with fun games and activities to help us learn and grow together. Have fun while deepening your relationship and gaining new tools to handle the ups and downs of toddler and preschool life!",
+    length: '45min',
+    time: '10:30am',
+    price: '15 for Milwaukee residents, 23 for non-residents',
+    color: 'mint',
+    location: "Beulah Brinton Community Center",
+    address: '2555 S Bay St, Milwaukee, WI 53207',
+    addressUrl: 'https://goo.gl/maps/SsE58GLLf9pQC2MFA',
+    picture: '../assets/yb_1.jpg',
+    signUp: false,
+    url: 'https://web2.myvscloud.com/wbwsc/wimilwaukeewt.wsc/search.html?Action=Start&SubAction=&_csrf_token=8753b73fee1c2001d2ec2ef446fa5643296e8e5a1d44a55ab30691d4033bf3c9&module=global&keyword=yoga+buddies&keywordoption=Match+All&spotsavailable=&dayoption=All&timeblock=&beginyear=&display=Detail&search=yes&page=3&multiselectlist_value=&globalwebsearch_buttonsearch=yes',
+    classDates: [] = []
+  }
+]
--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,6 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { OnInit, Component } from '@angular/core';
 import { formatDate } from '@angular/common';
-// import { DatePipe } from '@angular/common';
 import { currentOfferings } from '../app.offerings';
 
 @Component({
@@ -8,115 +7,37 @@ import { currentOfferings } from '../app.offerings';
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.scss']
 })
-export class HomeComponent {
-  // currentOfferings = currentOfferings;
-  public offerings = currentOfferings;
+export class HomeComponent implements OnInit {
+  public currentEvents = currentOfferings;
   public upcomingEvents: any = [];
 
-  // test = currentOfferings;
-
-  // public comingClasses = [
-  //   {
-  //     className: 'Yoga with Your Little',
-  //     classTime: '9am',
-  //     location: 'Integrative Wellness - Bay View',
-  //     classDates: [
-  //       '09/17/2022',
-  //       '10/01/2022',
-  //       '08/18/2022', //test
-  //       '10/15/2022',
-  //       '10/29/2022'
-  //     ]
-  //   },
-  //   {
-  //     className: 'Yoga for Parents & Caregivers',
-  //     classTime: '9am',
-  //     location: 'Heart Revival Center - St Francis',
-  //     classDates: [
-  //       '09/23/2022',
-  //       '09/30/2022',
-  //       '10/07/2022',
-  //       '08/16/2022', //test
-  //       '10/14/2022',
-  //       '10/21/2022',
-  //       '10/28/2022'
-  //     ]
-  //   },
-  // ]
-  public upcomingClasses = [
-    { style: 'green', classDate: '09/17/2022', className: 'Yoga with Your Little', classTime: '9am', location: 'Integrative Wellness - Bay View', signUp: true, url: "https://calendly.com/cultivateyogamke/yogawithyourlittle" },
-    { style: 'green', classDate: '10/15/2022', className: 'Yoga with Your Little', classTime: '9am', location: 'Integrative Wellness - Bay View', signUp: true, url: "https://calendly.com/cultivateyogamke/yogawithyourlittle" },
-    { style: 'green', classDate: '10/01/2022', className: 'Yoga with Your Little', classTime: '9am', location: 'Integrative Wellness - Bay View', signUp: true, url: "https://calendly.com/cultivateyogamke/yogawithyourlittle" },
-    { style: 'orange', classDate: '09/23/2022', className: 'Yoga for Parents & Caregivers', classTime: '9am', location: 'Heart Revival Center - St Francis', signUp: true, url: "https://calendly.com/cultivateyogamke/yogaforparents" },
-    { style: 'orange', classDate: '09/30/2022', className: 'Yoga for Parents & Caregivers', classTime: '9am', location: 'Heart Revival Center - St Francis', signUp: true, url: "https://calendly.com/cultivateyogamke/yogaforparents" },
-    { style: 'orange', classDate: '10/07/2022', className: 'Yoga for Parents & Caregivers', classTime: '9am', location: 'Heart Revival Center - St Francis', signUp: true, url: "https://calendly.com/cultivateyogamke/yogaforparents" },
-    { style: 'orange', classDate: '10/14/2022', className: 'Yoga for Parents & Caregivers', classTime: '9am', location: 'Heart Revival Center - St Francis', signUp: true, url: "https://calendly.com/cultivateyogamke/yogaforparents" },
-    { style: 'orange', classDate: '10/21/2022', className: 'Yoga for Parents & Caregivers', classTime: '9am', location: 'Heart Revival Center - St Francis', signUp: true, url: "https://calendly.com/cultivateyogamke/yogaforparents" },
-    { style: 'orange', classDate: '10/28/2022', className: 'Yoga for Parents & Caregivers', classTime: '9am', location: 'Heart Revival Center - St Francis', signUp: true, url: "https://calendly.com/cultivateyogamke/yogaforparents" },
-    {
-      style: 'gold', classDate: '09/25/2022', className: "Kids' Yoga in the Garden", classTime: '10:30am', location: "Alice's Garden Urban Farm", signUp: false,
-      url: "https://www.facebook.com/events/3096932107190182/?ref=newsfeed"
-    }
-  ]
-
-  get sortedClasses() {
-    // remove dates in the past
-    this.upcomingClasses.forEach((item, index) => {
-      if (new Date(item.classDate) < new Date()) this.upcomingClasses.splice(index, 1);
-    });
-    // sort classes by date
-    return this.upcomingClasses.sort((a, b) => {
-      return <any>new Date(a.classDate) - <any>new Date(b.classDate);
-    });
-  }
-
   calendly(event: any) {
-    const month = formatDate(event.classDate, 'yyyy-MM', 'en-US');
+    const month = formatDate(event.date, 'yyyy-MM', 'en-US');
     window.Calendly.initPopupWidget({
-      url: event.url + '?hide_event_type_details=1&back=1&month=' + month + '&date=' + event.classDate
+      url: event.details.calendlyUrl + '?hide_event_type_details=1&back=1&month=' + month + '&date=' + event.date
+      // url: event.details.calendlyUrl
     });
   }
 
-  // ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-
-  removePastDays() {
-    console.log('removing past days now');
-    this.offerings.forEach((item) => {
+  ngOnInit() {
+    // remove days in the past
+    this.currentEvents.forEach((item) => {
       item.classDates.forEach((val, index) => {
         if (new Date(val) < new Date()) item.classDates.splice(index, 1);
       });
     });
-  }
-  // let newArray = arr.map((currentvalue,index,array)=>{
-  //   return Element of array
-  //   });
 
-  get filteredOfferings() {
-    var x: any = [];
-    var y: any = [];
-    this.removePastDays();
-    this.offerings.forEach((details) => {
-      details.classDates.forEach((date, dex) => {
-        x.push(details);
-        y.push(date);
-        // x[dex].classDates = 
-        // x[dex].classDates = date;
-      // x[dex].classDates = '';
-      
+    // for every class and then for every date of those classes, push details and dates to new lists
+    this.currentEvents.forEach((singleClass) => {
+      singleClass.classDates.forEach((eventDate, i) => {
+        this.upcomingEvents.push({ details: singleClass, date: eventDate });
       });
     });
-    x.forEach((offering: any, index:any) => {
-      // console.log('off', offering);
-      // console.log('ind', index);
-      // console.log('y', y[index]);
-      offering.classDates = y[index];
-      console.log('x', x);
-    });
 
-    return x;
-    // return this.upcomingEvents.sort((a: { date: string | number | Date; }, b: { date: string | number | Date; }) => {
-    //   return <any>new Date(a.date) - <any>new Date(b.date);
-    // });
+    // sort events chronologically
+    this.upcomingEvents.sort((a: { date: string | number | Date; }, b: { date: string | number | Date; }) => {
+      return <any>new Date(a.date) - <any>new Date(b.date);
+    });
   }
 }
 
