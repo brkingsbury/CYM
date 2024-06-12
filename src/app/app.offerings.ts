8af2f98a2cd6089269fb1afa6f7650f184d7994c
Brian <briankingsbury1@gmail.com>
2024-06-11 20:51:02 -0500

    Update classes

--- src/app/app.offerings.ts
+++ src/app/app.offerings.ts
@@ -24,45 +24,45 @@ export interface Offerings {
 }
 
 export const currentOfferings: Offerings[] = [
+  // {
+  //   name: 'Family Mindfulness Time',
+  //   type: 'Class',
+  //   ages: '0 - 6',
+  //   description: "A drop-in hour of mindfulness activities for little ones ages 6 and under and their parents/caregivers! We'll use the tools of yoga and mindfulness to practice skills like connecting with our bodies and feelings, strategies for emotion regulation, and ways to bond and connect with your child. Each class will combine a variety of games, activities, physical yoga poses, meditation and mindfulness techniques, stories, and crafts to help us learn and grow together. We will also take time to build community and make connections with each other. Come have fun and make friends while deepening your relationship and gaining new tools to handle the ups and downs of toddler and preschool life!" + "\n\n" + "No registration necessary - feel free to drop in!" + "\n\n" + "We are currently meeting in Humboldt Park near the corner of Pine and Montana, check social media for any weather updates or cancellations.",
+  //   time: new Date("2025-01-01 11:00"),
+  //   occurance: '2nd & 4th Wednesdays',
+  //   length: '1 hour',
+  //   price: 'FREE!',
+  //   color: 'mint',
+  //   location: "Bay View Community Center",
+  //   address: '1320 E Oklahoma Ave, Milwaukee, WI 53207',
+  //   addressUrl: 'https://maps.app.goo.gl/edkUAbetnnQ3TMP67',
+  //   picture: '../assets/class-photos/mindfulness.jpg',
+  //   altText: "A group of adults and children on yoga mats",
+  //   signUp: false,
+  //   dropIn: true,
+  //   url: '',
+  //   classDates: [] = [
+  //     '01/10/2024',
+  //     '01/24/2024',
+  //     '02/14/2024',
+  //     '02/28/2024',
+  //     '03/13/2024',
+  //     '03/27/2024',
+  //     '04/10/2024',
+  //     '04/24/2024',
+  //     '05/08/2024',
+  //     '05/22/2024'
+  //   ]
+  // },
   {
-    name: 'Family Mindfulness Time',
-    type: 'Class',
-    ages: '0 - 6',
-    description: "A drop-in hour of mindfulness activities for little ones ages 6 and under and their parents/caregivers! We'll use the tools of yoga and mindfulness to practice skills like connecting with our bodies and feelings, strategies for emotion regulation, and ways to bond and connect with your child. Each class will combine a variety of games, activities, physical yoga poses, meditation and mindfulness techniques, stories, and crafts to help us learn and grow together. We will also take time to build community and make connections with each other. Come have fun and make friends while deepening your relationship and gaining new tools to handle the ups and downs of toddler and preschool life!" + "\n\n" + "No registration necessary - feel free to drop in!" + "\n\n" + "We are currently meeting in Humboldt Park near the corner of Pine and Montana, check social media for any weather updates or cancellations.",
-    time: new Date("2025-01-01 11:00"),
-    occurance: '2nd & 4th Wednesdays',
-    length: '1 hour',
-    price: 'FREE!',
-    color: 'mint',
-    location: "Bay View Community Center",
-    address: '1320 E Oklahoma Ave, Milwaukee, WI 53207',
-    addressUrl: 'https://maps.app.goo.gl/edkUAbetnnQ3TMP67',
-    picture: '../assets/class-photos/mindfulness.jpg',
-    altText: "A group of adults and children on yoga mats",
-    signUp: false,
-    dropIn: true,
-    url: '',
-    classDates: [] = [
-      '01/10/2024',
-      '01/24/2024',
-      '02/14/2024',
-      '02/28/2024',
-      '03/13/2024',
-      '03/27/2024',
-      '04/10/2024',
-      '04/24/2024',
-      '05/08/2024',
-      '05/22/2024'
-    ]
-  },
-  {
-    name: "Mindful Member Mondays",
+    name: "Mindful Mondays",
     type: 'Class',
     ages: '0-6',
-    description: "Little ones and their parent/guardian(s) are invited to practice mindfulness through a variety of physical yoga poses, breath practices, stories, games, and activities to cultivate connection with the body, breath, emotions, and each other. All experiences and levels are welcome. This class is only for Betty Brinn members. Space is limited - please register to reserve your spot.",
+    description: "This class is for kids of all ages – parents/caregivers are encouraged to join as well! Each class will combine a variety of yoga poses, meditation and mindfulness practices, games, and activities to help us learn and grow together. This event will be held outdoors in Museum Center Park, weather-permitting. Participants are encouraged to bring yoga mats or blankets, sunscreen, and water.",
     length: '45min',
-    occurance: 'First Mondays',
-    time: new Date("2025-01-01 11:00"),
+    occurance: 'Every Monday',
+    time: new Date("2025-01-01 10:00"),
     price: 'Free, for members',
     color: 'orange',
     location: "Betty Brinn Children's Museum",
@@ -75,10 +75,15 @@ export const currentOfferings: Offerings[] = [
     picture: '../assets/class-photos/bb_y4families.jpg',
     altText: "An adult doing yoga outside",
     classDates: [] = [
-      '02/12/2024',
-      '03/04/2024',
-      '04/01/2024',
-      '05/06/2024'
+      '06/17/2024',
+      '06/24/2024',
+      '07/01/2024',
+      '07/08/2024',
+      '07/22/2024',
+      '07/29/2024',
+      '08/05/2024',
+      '08/12/2024'
+      
     ]
   },
   {
@@ -100,12 +105,14 @@ export const currentOfferings: Offerings[] = [
     dropIn: false,
     url: 'https://app.rockgympro.com/b/widget/?a=offering&offering_guid=c08634b933d345eaab99e85e3b7858d3&random=65072821480c7&iframeid=&mode=p',
     classDates: [] = [
-      '01/10/2024',
-      '02/14/2024',
-      '03/13/2024',
-      '04/10/2024',
-      '05/08/2024',
-      '06/12/2024'
+      '06/12/2024',
+      '07/10/2024',
+      '08/14/2024',
+      '09/11/2024',
+      // '10/09/2024',
+      // '11/13/2024',
+      // '12/11/2024'
+
     ]
   },
   // {
@@ -139,48 +146,48 @@ export const currentOfferings: Offerings[] = [
   //     '12/08/2023'
   //   ]
   // },
-  {
-    name: "Yoga Together",
-    ages: '2-6',
-    type: 'Event',
-    description: "Join Holly from Cultivate Yoga MKE in this class designed for little ones ages 2 to 6 and their parents/caregivers at Healium Hot Yoga on Sunday August 20th from 3:30-4:30 pm! Kids and their grown-ups will use physical poses, mindfulness practices, and games and activities to practice connecting with their bodies, breath, and with each other. " + "\n\n" + "Each child must attend with at least one adult. $15 per child. Adults are welcome to attend with multiple children - please email info@healiumhotyoga.com to arrange for additional child sign-up and payment. ",
-    length: '1 hour',
-    time: new Date("2025-03-24 15:30"),
-    occurance: 'Once',
-    price: '$15 per child',
-    color: 'gold',
-    location: "Healium Hot Yoga",
-    address: '2534 S Kinnickinnic Ave, Milwaukee, WI 53207',
-    addressUrl: 'https://goo.gl/maps/zj45aJbkE4VAWBcHA',
-    picture: '../assets/class-photos/y4p.jpg',
-    altText: "Kids doing yoga",
-    signUp: true,
-    dropIn: false,
-    url: 'https://www.healiumhotyoga.com/happenings/yogatogethermarch',
-    classDates: [] = [
-      '03/24/2024'
-    ]
-  },
-  {
-    name: "Yoga for Kids",
-    ages: '7-11',
-    type: 'Class',
-    description: "This class provides an opportunity for kids to move their bodies, release some energy, and relax their minds after a long day of learning at school! Each class will combine yoga poses (asana) and breathing techniques (pranayama) with fun games and activities to help kids strengthen physical and emotional skills alike. No prior experience is necessary - all are welcome to join us as we work on building confidence, appreciating our unique gifts, and fostering connection. Class is for kids only.",
-    length: '1 hour',
-    time: new Date("2025-01-01 17:00"),
-    occurance: 'Mondays starting 04/01',
-    price: '$14 for residents / $21 for non-residents',
-    color: 'gold',
-    location: "Beulah Brinton Community Center",
-    address: '2555 S Bay St, Milwaukee, WI 53207',
-    addressUrl: 'https://maps.app.goo.gl/pBxFA38uXFcSZLAj8',
-    picture: '../assets/class-photos/y4k.png',
-    altText: "Kids doing yoga",
-    signUp: true,
-    dropIn: false,
-    url: 'https://web2.myvscloud.com/wbwsc/wimilwaukeewt.wsc/search.html?Action=Start&SubAction=&_csrf_token=58dc32e4f06736bec4c9a41fb20eeec51ed43e8a77f5a1084276da77b594ed8e&module=global&keyword=Yoga+for+Kids&keywordoption=Match+All&spotsavailable=&dayoption=All&timeblock=&beginyear=&display=Detail&search=yes&page=1&multiselectlist_value=&globalwebsearch_buttonsearch=yes',
-    classDates: [] = [
-      '04/01/2024',
-    ]
-  }
+  // {
+  //   name: "Yoga Together",
+  //   ages: '2-6',
+  //   type: 'Event',
+  //   description: "Join Holly from Cultivate Yoga MKE in this class designed for little ones ages 2 to 6 and their parents/caregivers at Healium Hot Yoga on Sunday August 20th from 3:30-4:30 pm! Kids and their grown-ups will use physical poses, mindfulness practices, and games and activities to practice connecting with their bodies, breath, and with each other. " + "\n\n" + "Each child must attend with at least one adult. $15 per child. Adults are welcome to attend with multiple children - please email info@healiumhotyoga.com to arrange for additional child sign-up and payment. ",
+  //   length: '1 hour',
+  //   time: new Date("2025-03-24 15:30"),
+  //   occurance: 'Once',
+  //   price: '$15 per child',
+  //   color: 'gold',
+  //   location: "Healium Hot Yoga",
+  //   address: '2534 S Kinnickinnic Ave, Milwaukee, WI 53207',
+  //   addressUrl: 'https://goo.gl/maps/zj45aJbkE4VAWBcHA',
+  //   picture: '../assets/class-photos/y4p.jpg',
+  //   altText: "Kids doing yoga",
+  //   signUp: true,
+  //   dropIn: false,
+  //   url: 'https://www.healiumhotyoga.com/happenings/yogatogethermarch',
+  //   classDates: [] = [
+  //     '03/24/2024'
+  //   ]
+  // },
+  // {
+  //   name: "Yoga for Kids",
+  //   ages: '7-11',
+  //   type: 'Class',
+  //   description: "This class provides an opportunity for kids to move their bodies, release some energy, and relax their minds after a long day of learning at school! Each class will combine yoga poses (asana) and breathing techniques (pranayama) with fun games and activities to help kids strengthen physical and emotional skills alike. No prior experience is necessary - all are welcome to join us as we work on building confidence, appreciating our unique gifts, and fostering connection. Class is for kids only.",
+  //   length: '1 hour',
+  //   time: new Date("2025-01-01 17:00"),
+  //   occurance: 'Mondays starting 04/01',
+  //   price: '$14 for residents / $21 for non-residents',
+  //   color: 'gold',
+  //   location: "Beulah Brinton Community Center",
+  //   address: '2555 S Bay St, Milwaukee, WI 53207',
+  //   addressUrl: 'https://maps.app.goo.gl/pBxFA38uXFcSZLAj8',
+  //   picture: '../assets/class-photos/y4k.png',
+  //   altText: "Kids doing yoga",
+  //   signUp: true,
+  //   dropIn: false,
+  //   url: 'https://web2.myvscloud.com/wbwsc/wimilwaukeewt.wsc/search.html?Action=Start&SubAction=&_csrf_token=58dc32e4f06736bec4c9a41fb20eeec51ed43e8a77f5a1084276da77b594ed8e&module=global&keyword=Yoga+for+Kids&keywordoption=Match+All&spotsavailable=&dayoption=All&timeblock=&beginyear=&display=Detail&search=yes&page=1&multiselectlist_value=&globalwebsearch_buttonsearch=yes',
+  //   classDates: [] = [
+  //     '04/01/2024',
+  //   ]
+  // }
 ]
